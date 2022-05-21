0bdab0d25af7254b305ba231fb501d78bb54d74d
AliMasaoodi <33722769+AliMasaoodi@users.noreply.github.com>
2022-05-21 13:11:19 +0300

    Update and rename Histogram_Stretching_default.m to Histogram_Stretching_default_Custom.m

--- Histogram_Stretching_default_Custom.m
+++ Histogram_Stretching_default_Custom.m
@@ -1,6 +1,6 @@
 % using the default limits [0.01 0.99]:
 
-I = imread('c:\Myimage2.jpg');
+I = imread('c:\image.jpg');
 
 % to stretch contrast of an image, will use imadjust() function and call another function stretchlim():
 % J = imadjust(I,[low_in high_in],[low_out high_out])
@@ -9,6 +9,20 @@ I = imread('c:\Myimage2.jpg');
 % using the default limits [0.01 0.99], saturating the upper 1% and the lower 1%.
  
 J = imadjust(I,stretchlim(I),[]);
+ 
+subplot(2,2,1), imshow(I); title('Original Image');
+subplot(2,2,3), imshow(J); title('Stretched Contrast Image');
+subplot(2,2,2), imhist(I); title('Histogram of Original Image');
+subplot(2,2,4), imhist(J); title('Histogram of Stretched Contrast Image');
+
+
+% Now will see the difference buy using 0,1 limits:
+
+I = imread('c:\image.jpg');
+% to stretch contrast of an image, will use imadjust() function and call another function stretchlim():
+% J = imadjust(I,[low_in high_in],[low_out high_out])
+ 
+J = imadjust(I,[],[1,0]);
  
 subplot(2,2,1), imshow(I); title('Original Image');
 subplot(2,2,3), imshow(J); title('Stretched Contrast Image');
